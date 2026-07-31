c5a04f1fe1aad81fe311ffaea4b135318f277648
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-31 22:42:46 +0330

    Add prototype combat HUD

---
+++ Source/ARASH/Game/ArashHUD.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "ArashHUD.generated.h"
+
+UCLASS()
+class ARASH_API AArashHUD : public AHUD
+{
+    GENERATED_BODY()
+
+public:
+    virtual void DrawHUD() override;
+
+private:
+    void DrawBar(float X, float Y, float Width, float Height, float Alpha, const FLinearColor& FillColor) const;
+};
