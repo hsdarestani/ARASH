d13d8ab1dbf15adf7612eeddacc194eb30deaa0f
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-31 13:40:34 +0330

    feat: add player character interface

---
+++ Source/ARASH/Player/ArashCharacter.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ArashCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UArashBowComponent;
+
+UCLASS()
+class ARASH_API AArashCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AArashCharacter();
+
+    virtual void Tick(float DeltaSeconds) override;
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+    TObjectPtr<USpringArmComponent> CameraBoom;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+    TObjectPtr<UCameraComponent> Camera;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat")
+    TObjectPtr<UArashBowComponent> Bow;
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    void MoveForward(float Value);
+    void MoveRight(float Value);
+    void StartFire();
+    void StopFire();
+    void Dodge();
+    void UpdateAim();
+};
