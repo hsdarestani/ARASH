9e5bb3a56b82538f6cd0a0e5edab71fb699bf246
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-31 13:40:07 +0330

    feat: add bow charge component

---
+++ Source/ARASH/Combat/ArashBowComponent.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ArashBowComponent.generated.h"
+
+class AMythicArrowProjectile;
+
+UCLASS(ClassGroup=(Combat), meta=(BlueprintSpawnableComponent))
+class ARASH_API UArashBowComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UArashBowComponent();
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bow")
+    TSubclassOf<AMythicArrowProjectile> ProjectileClass;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bow", meta = (ClampMin = "0.1"))
+    float MaxChargeTime = 1.0f;
+
+    UFUNCTION(BlueprintCallable, Category = "Bow")
+    void StartCharge();
+
+    UFUNCTION(BlueprintCallable, Category = "Bow")
+    AMythicArrowProjectile* ReleaseArrow();
+
+    UFUNCTION(BlueprintPure, Category = "Bow")
+    float GetChargeAlpha() const;
+
+private:
+    bool bCharging = false;
+    float ChargeStartedAt = 0.0f;
+};
