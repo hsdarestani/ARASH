f8d78911b3ce9b8dc5909ab75f805fde10c59f9a
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-31 18:42:14 +0330

    fix: expose module root include path for UE 5.8

--- Source/ARASH/ARASH.Build.cs
+++ Source/ARASH/ARASH.Build.cs
@@ -1,10 +1,13 @@
 using UnrealBuildTool;
+using System.IO;
 
 public class ARASH : ModuleRules
 {
     public ARASH(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicIncludePaths.Add(ModuleDirectory);
 
         PublicDependencyModuleNames.AddRange(new string[]
         {
