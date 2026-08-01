c60baa8c6e039e3663f2c23296022ef5e56ac21b
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-01 12:24:08 +0330

    Auto-spawn generated environment manager

--- Source/ARASH/ARASH.cpp
+++ Source/ARASH/ARASH.cpp
@@ -1,4 +1,53 @@
 #include "ARASH.h"
+
+#include "Engine/World.h"
+#include "Environment/ArashEnvironmentManager.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ARASH, "ARASH");
+class FArashGameModule final : public FDefaultGameModuleImpl
+{
+public:
+    virtual void StartupModule() override
+    {
+        FDefaultGameModuleImpl::StartupModule();
+
+        WorldInitHandle = FWorldDelegates::OnPostWorldInitialization.AddRaw(
+            this,
+            &FArashGameModule::HandlePostWorldInitialization);
+    }
+
+    virtual void ShutdownModule() override
+    {
+        if (WorldInitHandle.IsValid())
+        {
+            FWorldDelegates::OnPostWorldInitialization.Remove(WorldInitHandle);
+            WorldInitHandle.Reset();
+        }
+
+        FDefaultGameModuleImpl::ShutdownModule();
+    }
+
+private:
+    void HandlePostWorldInitialization(
+        UWorld* World,
+        const UWorld::InitializationValues InitializationValues)
+    {
+        if (!World || !World->IsGameWorld())
+        {
+            return;
+        }
+
+        FActorSpawnParameters Params;
+        Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+        Params.ObjectFlags |= RF_Transient;
+
+        World->SpawnActor<AArashEnvironmentManager>(
+            FVector::ZeroVector,
+            FRotator::ZeroRotator,
+            Params);
+    }
+
+    FDelegateHandle WorldInitHandle;
+};
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FArashGameModule, ARASH, "ARASH");
